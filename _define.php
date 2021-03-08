3fc82a3025a9f145f7f16c275189d61ab86e35b5
Lomalarch <lomalarch@gmail.com>
2021-03-08 16:06:00 +0100

    Initial _define.php

---
+++ _define.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @brief CTA, a plugin for Dotclear 2
+ *
+ * @package Dotclear
+ * @subpackage Plugins
+ *
+ * @author Noé Cendrier
+ *
+ * @copyright Noé Cendrier noe@noecendrier.fr
+ * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+if (!defined('DC_RC_PATH')) {return;}
+
+$this->registerModule(
+    "Call to action (CTA)",                       // Name
+    "Easily insert buttons in your blog entries", // Description
+    "Noé Cendrier",                               // Author
+    '0.1',                                        // Version
+    [
+        'requires'    => [['core', '2.16']],
+        'permissions' => 'usage,contentadmin',
+        'type'        => 'plugin',
+        'priority'    => 1001,                                        // Must be higher than dcLegacyEditor priority (ie 1000)
+        'support'     => 'https://github.com/Lomalarch/??????????', // Support URL
+        // 'details'     => 'https://noecendrier.fr', // Details
+/*        'settings'    => [                                            // Settings
+            'self' => '',
+            'blog' => '#params.????????',
+            'pref' => '#user-options.????????'
+        ]*/
+    ]
+);
+
